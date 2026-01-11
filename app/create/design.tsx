24120351696ff4a1797116e452e26db3d3db7aa2
Ibrahim Inelhaj <ibrahim.inelhaj-prestataire@laposte.fr>
2026-01-11 18:55:51 +0100

    some fixes

--- app/create/design.tsx
+++ app/create/design.tsx
@@ -363,7 +363,7 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
         elevation: 5,
-        width: '90%',
+        width: '100%',
         maxWidth: 400,
     },
     confirmMessage: {
